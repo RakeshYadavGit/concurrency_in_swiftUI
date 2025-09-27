0fcfa6c132f82a0fe1aff1909e58ebeebcbcb06f
Rakesh Yadav <rryadav142001@gmail.com>
2025-09-27 20:40:38 +0530

    Initial project setup for ConcurrencyInSwift app
    Added Xcode project files, SwiftUI app entry point, main ContentView, asset catalogs, and initial unit/UI test stubs for the new ConcurrencyInSwift iOS application.

---
+++ ConcurrencyInSwift/ConcurrencyInSwift/ConcurrencyInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyInSwiftApp.swift
+//  ConcurrencyInSwift
+//
+//  Created by Rakesh Yadav on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcurrencyInSwift/ConcurrencyInSwiftTests/ConcurrencyInSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyInSwiftTests.swift
+//  ConcurrencyInSwiftTests
+//
+//  Created by Rakesh Yadav on 27/09/25.
+//
+
+import Testing
+@testable import ConcurrencyInSwift
+
+struct ConcurrencyInSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
