d9b1b62e3e31889e06fa95e0b1206b117fe88c76
Rakesh Yadav <rryadav142001@gmail.com>
2025-09-28 20:01:37 +0530

    Update NetworkManager.swift

--- ConcurrencyInSwift/ConcurrencyInSwift/Manager/NetworkManager.swift
+++ ConcurrencyInSwift/ConcurrencyInSwift/Manager/NetworkManager.swift
@@ -31,4 +31,45 @@ class NetworkManager {
         
         return newsArticleResponse?.articles ?? []
     }
+    
+    func fetchNewsWithAsync(by sourceId: String, url: URL?) async throws -> [NewsArticle] {
+       try await withUnsafeThrowingContinuation { continuation in
+            self.fetchNews(by: sourceId, url: url) { result in
+                switch result {
+                case .success(let newsArticles):
+                    continuation.resume(returning: newsArticles)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    func fetchNews(by sourceId: String, url: URL?, completion: @escaping (Result<[NewsArticle], NetworkError>) -> Void) {
+        
+        guard let url = url else {
+            completion(.failure(.badUrl))
+            return
+        }
+            
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            
+            guard let data = data, error == nil else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let newsArticleResponse = try JSONDecoder().decode(NewsArticleResponse.self, from: data)
+                completion(.success(newsArticleResponse.articles ?? []))
+            } catch {
+                completion(.failure(.invalidData))
+                
+                print(error)
+            }
+            
+            
+        }.resume()
+        
+    }
 }
